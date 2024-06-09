153debddec7c81d1048cc0906a5613cc375be7cc
ZenekeZene <hectorvillarm@gmail.com>
2024-06-09 17:21:25 +0200

    [refactor]: changed hardcoded variables of store

--- src/ui/views/TryAgain/TryAgain.tsx
+++ src/ui/views/TryAgain/TryAgain.tsx
@@ -1,24 +1,29 @@
-import { MdOutlineReplay } from "react-icons/md";
-import { Progression } from "@/ui/components/Progression/Progression"
-import './TryAgain.scss';
-
-const totalLevels = 20
-const currentLevel = 3
+import { MdOutlineReplay as ReplayIcon } from "react-icons/md";
+import { useShallow } from "zustand/react/shallow";
+import { Progression } from "@/ui/components/Progression/Progression";
+import { useStore } from "@/store/store";
+import "./TryAgain.scss";
 
 const TryAgainView = () => {
-	return (
-		<article className="try-again view">
-			<h2 className="try-again__subtitle">You almost got in</h2>
-			<h1 className="try-again__title">Try Again</h1>
-			<button className="try-again__replay button --square">
-				<MdOutlineReplay />
-			</button>
-			<Progression
-				currentLevel={currentLevel}
-				totalLevels={totalLevels}
-			/>
-		</article>
-	);
+  const state = useStore(
+    useShallow(({ currentLevel, levels }) => ({
+      currentLevel,
+      totalLevels: levels.length,
+    })));
+
+  return (
+    <article className="try-again view">
+      <h2 className="try-again__subtitle">You almost got in</h2>
+      <h1 className="try-again__title">Try Again</h1>
+      <button className="try-again__replay button --square">
+        <ReplayIcon />
+      </button>
+      <Progression
+        currentLevel={state.currentLevel}
+        totalLevels={state.totalLevels}
+      />
+    </article>
+  );
 };
 
 export { TryAgainView };
