1097681178be4f7113675ae928f5fcbca2935ab1
ZenekeZene <hectorvillarm@gmail.com>
2024-06-09 16:49:13 +0200

    [test]: added progression test

--- src/ui/components/Progression/Progression.tsx
+++ src/ui/components/Progression/Progression.tsx
@@ -1,34 +1,41 @@
-import './Progression.scss'
+import "./Progression.scss";
+
+const errorMessage = `Error with Progression component.
+	CurrentLevel can not be major than totalLevels`;
 
 interface Props {
-	currentLevel: number,
-	totalLevels: number,
+  currentLevel: number;
+  totalLevels: number;
 }
 
 const ProgressionList = ({ currentLevel, totalLevels }: Props) => {
-	const getClassname = (index: number) => currentLevel > index ? '--completed': ''
+  const isCompleted = (index: number) =>
+    currentLevel > index ? "--completed" : "";
 
-	return (
-		<ul className="progression__list">
-			{ new Array(totalLevels).fill('').map((_, index) => (
-				<li className={`progression__item ${getClassname(index)}`}>{}</li>
-			))}
-		</ul>
-	)
-}
+  return (
+    <ul className="progression__list">
+      {new Array(totalLevels).fill("").map((_, index) => (
+        <li className={`progression__item ${isCompleted(index) ? '--completed' : ''}`}
+					aria-label={`Level ${index} ${isCompleted(index) ? 'unfinished ': 'completed' }`}
+				>{}</li>
+      ))}
+    </ul>
+  );
+};
 
 const Progression = ({ currentLevel, totalLevels }: Props) => {
-	const percent = (currentLevel / totalLevels) * 100
+  if (currentLevel > totalLevels) throw new Error(errorMessage);
+  const percent = Math.floor((currentLevel / totalLevels) * 100);
 
-	return (
-		<article className="progression">
-			<p className="progression__text">Game progression: <span className="progression__percent">{percent}%</span></p>
-			<ProgressionList
-				currentLevel={currentLevel}
-				totalLevels={totalLevels}
-			/>
-		</article>
-	);
+  return (
+    <article className="progression">
+      <p className="progression__text">
+        Game progression:
+        <span className="progression__percent">{percent}%</span>
+      </p>
+      <ProgressionList currentLevel={currentLevel} totalLevels={totalLevels} />
+    </article>
+  );
 };
 
 export { Progression };
