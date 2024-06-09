b7e1437d8bdb605f716e65b26b1fda600a79af6a
ZenekeZene <hectorvillarm@gmail.com>
2024-06-09 12:23:38 +0200

    [test]: added Tutorial spec

---
+++ src/ui/views/Tutorial/Tutorial.spec.tsx
@@ -0,0 +1,36 @@
+import { describe, test, expect, afterEach } from 'bun:test'
+import { MemoryRouter } from "react-router-dom";
+import { screen, render, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { TutorialView } from './Tutorial'
+
+describe('Tutorial view', () => {
+	afterEach(() => {
+		cleanup()
+	})
+
+	test(`if the user clicks on the view,
+		then she is directed to the game`, async () => {
+		const user = userEvent.setup()
+		render(<TutorialView />, { wrapper: MemoryRouter })
+
+		const title = screen.getByTestId('tutorial')
+		await user.click(title)
+
+		const expected = screen.getByTestId('location-display')
+		expect(expected.textContent).toBe('/game')
+	})
+
+	test(`if the user clicks on the link,
+		then she is directed to the game`, async () => {
+		const user = userEvent.setup()
+		render(<TutorialView />, { wrapper: MemoryRouter })
+
+		const skipButton = screen.getByRole('link', { name: /That's all!/i })
+		await user.click(skipButton)
+
+		const expected = screen.getByTestId('location-display')
+		expect(expected.textContent).toBe('/game')
+	})
+
+})
--- src/ui/views/Tutorial/Tutorial.tsx
+++ src/ui/views/Tutorial/Tutorial.tsx
@@ -1,7 +1,13 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { FaAngleRight } from "react-icons/fa";
 import { useClassnameOfRoute } from '@/ui/hooks/useClassnameOfRoute/useClassnameOfRoute';
 import './Tutorial.scss';
+
+export const LocationDisplay = () => {
+  const location = useLocation()
+
+  return <div data-testid="location-display">{location.pathname}</div>
+}
 
 const TutorialView = () => {
 	useClassnameOfRoute()
@@ -11,7 +17,7 @@ const TutorialView = () => {
 		navigate('/game')
 	}
 	return (
-		<aside onClick={skipTutorial} className="tutorial">
+		<aside onClick={skipTutorial} className="tutorial" data-testid="tutorial">
 			<h1 className="tutorial__title">MIX THE COLORS TO MATCH THE SAMPLES</h1>
 			<section className="tutorial__graphic tutorial-graphic">
 				<div className="tutorial-graphic__swatch"></div>
@@ -20,6 +26,7 @@ const TutorialView = () => {
 				<div className="tutorial-graphic__base"></div>
 			</section>
 			<Link to="/game" className="tutorial__next">That's all! Shall we get started?<FaAngleRight /></Link>
+			<LocationDisplay />
 		</aside>
 	);
 };
