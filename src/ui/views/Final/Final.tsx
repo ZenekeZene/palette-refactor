647379521e01bea666e67611058fbacc432389ba
ZenekeZene <hectorvillarm@gmail.com>
2024-06-14 17:54:47 +0200

    [feat]: integrated store with final view

--- src/ui/views/Final/Final.tsx
+++ src/ui/views/Final/Final.tsx
@@ -1,22 +1,32 @@
-import './Final.scss'
+import { useStore } from "@/store/store"
+import { useShallow } from "zustand/react/shallow"
+import "./Final.scss"
 
 const FinalView = () => {
-	return (
-		<article className="final view">
-			<header className="final__header">
-			</header>
-			<section className="final__body">
-				<h1 className="final__title">Well done!</h1>
-				<h2 className="final__subtitle">You have finished with <span className="final__score">1000 points at level</span></h2>
-				<div className="final__level">
-					<p className="final__level-score">3</p>
-					<img className="final__laurel" src="/laurel.svg" alt="" />
-					<a className="final__share">SHARE YOUR RECORD</a>
-				</div>
-			</section>
-			<img className="final__illustration" src="/pet.png" alt="" />
-		</article>
-	);
-};
+  const state = useStore(
+		useShallow(({ currentLevel, score }) => ({
+			currentLevel,
+			score,
+		})))
 
-export { FinalView };
+  return (
+    <article className="final view">
+      <header className="final__header"></header>
+      <section className="final__body">
+        <h1 className="final__title">Well done!</h1>
+        <h2 className="final__subtitle">
+          You have finished with{" "}
+          <span className="final__score">{state.score} points at level</span>
+        </h2>
+        <div className="final__level">
+          <p className="final__level-score">{state.currentLevel}</p>
+          <img className="final__laurel" src="/laurel.svg" alt="" />
+          <a className="final__share">SHARE YOUR RECORD</a>
+        </div>
+      </section>
+      <img className="final__illustration" src="/pet.png" alt="" />
+    </article>
+  )
+}
+
+export { FinalView }
