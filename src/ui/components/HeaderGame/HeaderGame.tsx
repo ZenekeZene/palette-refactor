432a70e7559ae5067c6e76cf843a333466e87a30
ZenekeZene <hectorvillarm@gmail.com>
2024-06-14 18:42:15 +0200

    [feat]: added header game

---
+++ src/ui/components/HeaderGame/HeaderGame.tsx
@@ -0,0 +1,31 @@
+import { IoChevronBack as IconBack } from "react-icons/io5"
+import { IconHeart } from "../IconHearth/IconHearth";
+import './HeaderGame.scss'
+
+interface HeaderGameProps {
+	currentLevel: number;
+	score: number;
+	lives: number;
+	onBack: () => void;
+}
+
+const HeaderGame = (props: HeaderGameProps) => {
+	return (
+		<header className="header-game">
+			<div className="header-game__back">
+				<IconBack />
+			</div>
+			<div className="header-game__level">
+				<span>Level {props.currentLevel}</span>
+			</div>
+			<div className="header-game__score">
+				<span>{props.score}</span>
+			</div>
+			<div className="header-game__lives">
+				{props.lives}<IconHeart />
+			</div>
+		</header>
+	)
+}
+
+export { HeaderGame }
