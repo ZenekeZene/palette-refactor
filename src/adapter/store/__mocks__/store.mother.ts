84e9e7c3b18a5360f034798dc382cb80d43c3d36
Zeneke <hectorvillarm@gmail.com>
2024-06-25 14:52:36 +0200

    [test]: added object mother

--- src/adapter/store/__mocks__/store.mother.ts
+++ src/adapter/store/__mocks__/store.mother.ts
@@ -1,39 +1,51 @@
+import { Mock } from 'vitest'
 import { State } from '@/adapter/store/store.types'
 import { LevelsCollection } from '@/domain/Level/LevelsCollection'
 import { Level } from '@/domain/Level/Level'
-import { Quote } from '@/domain/Quote/Quote'
-import { QuotesCollection } from '@/domain/Quote/QuotesCollection'
+import { QuoteProps } from '@/domain/Quote/Quote'
 import { GameSession } from '@/domain/GameSession/GameSession'
 import { createStore } from '@/adapter/store/useStore'
+import { StoreBuilder } from '@/adapter/store/__mocks__/store.builder'
 
 class StoreMother {
-  private static createInitialState(payload?: any): State {
+  public static minimalStore(useStore: Mock<any, any>): void {
+    const builder = new StoreBuilder()
     const levels = new LevelsCollection()
     levels.add(Level.fromPrimitive({ numberOfChips: 0 }))
-    const quotes = QuotesCollection.fromArray([{ text: 'quote', author: 'author' }])
-    const quote = payload && Quote.fromPrimitives(payload)
-    const gameSession = GameSession.fromPrimitives({ lives: 0, score: 0, level: 0, bonus: 0 })
-
-    return {
-      gameSession,
-      levels,
-      tutorialIsWatched: false,
-      quotes,
-      quote,
-      ...payload,
-    }
+    builder.withLevels(levels)
+    const gameSession = GameSession.fromPrimitives({ lives: 10, score: 0, level: 0, bonus: 0 })
+    builder.withGameSession(gameSession)
+    StoreMother.createStore(useStore, builder.currentState)
   }
 
-  static createStore(payload?: any) {
-    const initialState = StoreMother.createInitialState(payload)
-    const store = createStore(initialState)
-    return (selector: Function) => selector(store.getState())
+  public static storeWithQuote(useStore: Mock<any, any>, quote:QuoteProps): void {
+    const builder = new StoreBuilder()
+    const levels = new LevelsCollection()
+    levels.add(Level.fromPrimitive({ numberOfChips: 0 }))
+    builder.withLevels(levels)
+    const gameSession = GameSession.fromPrimitives({ lives: 10, score: 0, level: 0, bonus: 0 })
+    builder.withGameSession(gameSession)
+    builder.withQuote(quote)
+    StoreMother.createStore(useStore, builder.currentState)
+  }
+
+  public static storeMultipleLevels(useStore: Mock<any, any>, { level, levelsCount }: { level: number, levelsCount: number }): void {
+    const builder = new StoreBuilder()
+    const levels = new LevelsCollection()
+    for (let i = 0; i < levelsCount; i++) {
+      levels.add(Level.fromPrimitive({ numberOfChips: 0 }))
+    }
+    builder.withLevels(levels)
+    const gameSession = GameSession.fromPrimitives({ lives: 1, score: 0, level, bonus: 0 })
+    builder.withGameSession(gameSession)
+    StoreMother.createStore(useStore, builder.currentState)
+  }
+
+  private static createStore(useStore: Mock<any, any>, state: State): void {
+    const store = createStore(state)
+    const mocked = (selector: Function) => selector(store.getState())
+    useStore.mockImplementation(mocked)
   }
 }
 
 export { StoreMother }
-
-export const mockStore = (useStore: any, payload?: any) => {
-  const store = StoreMother.createStore(payload)
-  useStore.mockImplementation(store)
-}
