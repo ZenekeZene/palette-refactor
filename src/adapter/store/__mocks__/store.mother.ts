e0ba374bfbf048cc98fdca720891edf7a900474c
Zeneke <hectorvillarm@gmail.com>
2024-06-26 20:07:48 +0200

    [refactor]: added intermediate functions

--- src/adapter/store/__mocks__/store.mother.ts
+++ src/adapter/store/__mocks__/store.mother.ts
@@ -7,45 +7,86 @@ import { GameSession } from '@/domain/GameSession/GameSession'
 import { createStore } from '@/adapter/store/useStore'
 import { StoreBuilder } from '@/adapter/store/__mocks__/store.builder'
 
-class StoreMother {
-  public static minimalStore(useStore: Mock<any, any>): void {
-    const builder = new StoreBuilder()
+export class StoreMother {
+  private static createDefaultLevels(): LevelsCollection {
     const levels = new LevelsCollection()
-    levels.add(Level.fromPrimitive({ numberOfChips: 0 }))
-    builder.withLevels(levels)
-    const gameSession = GameSession.fromPrimitives({ lives: 10, score: 0, level: 0, bonus: 0 })
-    builder.withGameSession(gameSession)
-    StoreMother.createStore(useStore, builder.currentState)
-  }
-
-  public static storeWithQuote(useStore: Mock<any, any>, quote:QuoteProps): void {
-    const builder = new StoreBuilder()
-    const levels = new LevelsCollection()
-    levels.add(Level.fromPrimitive({ numberOfChips: 0 }))
-    builder.withLevels(levels)
-    const gameSession = GameSession.fromPrimitives({ lives: 10, score: 0, level: 0, bonus: 0 })
-    builder.withGameSession(gameSession)
-    builder.withQuote(quote)
-    StoreMother.createStore(useStore, builder.currentState)
-  }
-
-  public static storeMultipleLevels(useStore: Mock<any, any>, { level, levelsCount }: { level: number, levelsCount: number }): void {
-    const builder = new StoreBuilder()
-    const levels = new LevelsCollection()
-    for (let i = 0; i < levelsCount; i++) {
+    const { LEVELS_COUNT } = StoreMother.DEFAULT
+    for (let i = 0; i < LEVELS_COUNT; i++) {
       levels.add(Level.fromPrimitive({ numberOfChips: 0 }))
     }
-    builder.withLevels(levels)
-    const gameSession = GameSession.fromPrimitives({ lives: 1, score: 0, level, bonus: 0 })
-    builder.withGameSession(gameSession)
-    StoreMother.createStore(useStore, builder.currentState)
+    return levels
   }
 
-  private static createStore(useStore: Mock<any, any>, state: State): void {
+  private static createDefaultGameSession(): GameSession {
+    const { LIVES, SCORE, LEVEL, BONUS } = StoreMother.DEFAULT
+    return GameSession.fromPrimitives({
+      lives: LIVES,
+      score: SCORE,
+      level: LEVEL,
+      bonus: BONUS,
+    })
+  }
+
+  private static createDefaultBuilder(): StoreBuilder {
+    const builder = new StoreBuilder()
+    builder.withLevels(StoreMother.createDefaultLevels())
+    builder.withGameSession(StoreMother.createDefaultGameSession())
+    return builder
+  }
+
+  private static createStore(
+    useStore: StoreMother.UseStore,
+    state: State
+  ): void {
     const store = createStore(state)
     const mocked = (selector: Function) => selector(store.getState())
     useStore.mockImplementation(mocked)
   }
+
+  public static minimalStore(useStore: StoreMother.UseStore): void {
+    const builder = StoreMother.createDefaultBuilder()
+    StoreMother.createStore(useStore, builder.currentState)
+  }
+
+  public static storeWithQuote(
+    useStore: StoreMother.UseStore,
+    quote: QuoteProps
+  ): void {
+    const builder = StoreMother.createDefaultBuilder()
+    builder.withQuote(quote)
+    StoreMother.createStore(useStore, builder.currentState)
+  }
+
+  public static storeMultipleLevels(
+    useStore: StoreMother.UseStore,
+    options: StoreMother.MultipleLevels
+  ): void {
+    const builder = new StoreBuilder()
+    const levels = new LevelsCollection()
+    for (let i = 0; i < options.levelsCount; i++) {
+      levels.add(Level.fromPrimitive({ numberOfChips: 0 }))
+    }
+    builder.withLevels(levels)
+    const { LIVES, SCORE, BONUS } = StoreMother.DEFAULT
+    const gameSession = GameSession.fromPrimitives({
+      lives: LIVES,
+      score: SCORE,
+      bonus: BONUS,
+      level: options.level,
+    })
+    builder.withGameSession(gameSession)
+    StoreMother.createStore(useStore, builder.currentState)
+  }
 }
 
-export { StoreMother }
+export namespace StoreMother {
+  export enum DEFAULT {
+    LIVES = 10,
+    LEVELS_COUNT = 1,
+    LEVEL = 0,
+    BONUS = 0,
+    SCORE = 0,
+  }
+  export type MultipleLevels = { level: number; levelsCount: number }
+  export type UseStore = Mock<any, any>
+}
