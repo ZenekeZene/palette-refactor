cdd16b6751fb35169b3078f239c4fd3323c631fb
Zeneke <hectorvillarm@gmail.com>
2024-06-27 10:25:25 +0200

    [test]: added useClassnameOfRoute spec

---
+++ src/ui/hooks/useClassnameOfRoute/useClassnameOfRoute.spec.tsx
@@ -0,0 +1,19 @@
+import { describe, test, expect, vi } from 'vitest'
+import { useClassnameOfRoute } from "./useClassnameOfRoute"
+
+const useLocation = vi.hoisted(() => vi.fn())
+vi.mock('react-router-dom', () => ({ useLocation }))
+
+describe('useClassnameOfRoute', () => {
+  test(`given a location, the body have a classname with the pathname
+    and his suffix `, () => {
+    const spy = vi.spyOn(document.body, 'className', 'set')
+    const pathname = '/home'
+    const expected = 'home-view'
+    useLocation.mockReturnValue({ pathname })
+
+    useClassnameOfRoute()
+
+    expect(spy).toHaveBeenCalledWith(expected)
+  })
+})
