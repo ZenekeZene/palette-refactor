be32ec216febb72d49ce1d696ab2f4eb94504254
Zeneke <hectorvillarm@gmail.com>
2024-06-30 23:45:26 +0200

    [tools]: fix config path of vitest

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -7,7 +7,7 @@ export default mergeConfig(
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: ['./setup-vitest.ts'],
+      setupFiles: ['../../config/setup-vitest.ts'],
     },
   })
 )
