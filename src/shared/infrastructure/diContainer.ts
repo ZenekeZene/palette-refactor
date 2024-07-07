a2dbc27381b5aef9a00c0b1fa6e09e1d2f241c5f
Zeneke <hectorvillarm@gmail.com>
2024-07-07 21:46:03 +0200

    [chore]: inferred types in diContainer

--- src/shared/infrastructure/diContainer.ts
+++ src/shared/infrastructure/diContainer.ts
@@ -1,15 +1,9 @@
 import { container } from "tsyringe"
 import { Types } from "./identifiers"
 
-import { IPlayerLoaderRepository } from "@gameContext/player/domain/IPlayerLoaderRepository"
-import { IQuotesLoaderRepository } from "@gameContext/quote/domain/IQuotesLoaderRepository"
-import { ILevelsLoaderRepository } from "@gameContext/level/domain/ILevelsLoaderRepository"
-import { ILevelsRepository } from "@gameContext/level/domain/ILevelsRepository"
-import { IPlayerRepository } from "@gameContext/player/domain/IPlayerRepository"
-
-import { LoadPlayerUseCase, LoadPlayerUseCaseType } from "@gameContext/player/application/loadPlayer.usecase"
-import { LoadLevelsUseCase, LoadLevelsUseCaseType } from "@gameContext/level/application/loadLevels.usecase"
-import { LoadQuotesUseCase, LoadQuotesUseCaseType } from "@gameContext/quote/application/loadQuotes.usecase"
+import { LoadPlayerUseCase } from "@gameContext/player/application/loadPlayer.usecase"
+import { LoadLevelsUseCase } from "@gameContext/level/application/loadLevels.usecase"
+import { LoadQuotesUseCase } from "@gameContext/quote/application/loadQuotes.usecase"
 
 import { PlayerLoaderRepository } from "@gameContext/player/infrastructure/PlayerLoaderRepository"
 import { QuotesRepository } from "@gameContext/quote/infrastructure/QuotesRepository"
@@ -17,13 +11,13 @@ import { LevelsLoaderFromFileRepository } from "@gameContext/level/infrastructur
 import { LevelsInMemoryRepository } from "@gameContext/level/infrastructure/LevelsInMemoryRepository"
 import { PlayerInMemoryRepository } from "@gameContext/player/infrastructure/PlayerInMemoryRepository"
 
-container.registerSingleton<IPlayerLoaderRepository>(Types.IPlayerLoaderRepository, PlayerLoaderRepository)
-container.registerSingleton<IQuotesLoaderRepository>(Types.IQuotesLoaderRepository, QuotesRepository)
-container.registerSingleton<ILevelsLoaderRepository>(Types.ILevelsLoaderRepository, LevelsLoaderFromFileRepository)
+container.registerSingleton(Types.IPlayerLoaderRepository, PlayerLoaderRepository)
+container.registerSingleton(Types.IQuotesLoaderRepository, QuotesRepository)
+container.registerSingleton(Types.ILevelsLoaderRepository, LevelsLoaderFromFileRepository)
 
-container.registerSingleton<LoadPlayerUseCaseType>(Types.LoadPlayerUseCase, LoadPlayerUseCase)
-container.registerSingleton<LoadQuotesUseCaseType>(Types.LoadQuotesUseCase, LoadQuotesUseCase)
-container.registerSingleton<LoadLevelsUseCaseType>(Types.LoadLevelsUseCase, LoadLevelsUseCase)
+container.registerSingleton(Types.LoadPlayerUseCase, LoadPlayerUseCase)
+container.registerSingleton(Types.LoadQuotesUseCase, LoadQuotesUseCase)
+container.registerSingleton(Types.LoadLevelsUseCase, LoadLevelsUseCase)
 
-container.registerSingleton<ILevelsRepository>(Types.ILevelsRepository, LevelsInMemoryRepository)
-container.registerSingleton<IPlayerRepository>(Types.IPlayerRepository, PlayerInMemoryRepository)
+container.registerSingleton(Types.ILevelsRepository, LevelsInMemoryRepository)
+container.registerSingleton(Types.IPlayerRepository, PlayerInMemoryRepository)
