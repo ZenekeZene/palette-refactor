f732ae6a499ae718c30107e1738d77351e668548
Zeneke <hectorvillarm@gmail.com>
2024-07-28 21:55:19 +0200

    [feat]: UseCase utils with sync response

--- src/shared/domain/utils/UseCase.ts
+++ src/shared/domain/utils/UseCase.ts
@@ -2,5 +2,5 @@ import type { Request } from '@gameContext/shared/domain/utils/Request'
 import type { Response } from '@gameContext/shared/domain/utils/Response'
 
 export interface UseCase<Req extends Request, Res extends Response> {
-  execute: (request: Req) => Promise<Res>
+  execute: (request: Req) => Promise<Res> | Res
 }
