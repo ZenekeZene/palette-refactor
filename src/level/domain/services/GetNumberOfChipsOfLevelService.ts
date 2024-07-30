d0c670158292e0bbce413ebab5e7e4327b94f6e1
Zeneke <hectorvillarm@gmail.com>
2024-07-30 15:32:57 +0200

    [feat]: created GetNumberOfChipsOfLevelService

---
+++ src/level/domain/services/GetNumberOfChipsOfLevelService.ts
@@ -0,0 +1,27 @@
+import { LevelsRepository } from '../repositories/LevelsRepository'
+import { LevelsCollectionId } from '../LevelsCollectionId'
+import { LevelId } from '../models/level/LevelId'
+import { LevelsCollectionNotFoundException } from '../exceptions/LevelsCollectionNotFoundException'
+import { LevelNotFoundException } from '../exceptions/LevelNotFoundException'
+
+export class GetNumberOfChipsOfLevelService {
+  constructor(private levelsRepository: LevelsRepository) {}
+
+  async findLevel(
+    levelsCollectionId: LevelsCollectionId,
+    levelId: LevelId,
+  ): Promise<number> {
+    const levelsCollection =
+      await this.levelsRepository.searchById(levelsCollectionId)
+    if (!levelsCollection) {
+      throw new LevelsCollectionNotFoundException(levelsCollectionId)
+    }
+
+    const level = levelsCollection.searchLevelById(levelId)
+    if (!level) {
+      throw new LevelNotFoundException(levelsCollectionId, levelId)
+    }
+
+    return level.getNumberOfChips().valueOf()
+  }
+}
