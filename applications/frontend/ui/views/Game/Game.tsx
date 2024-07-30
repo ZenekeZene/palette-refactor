1ca97fd1d94b9cc694d518021d6a367a1e3708dd
Zeneke <hectorvillarm@gmail.com>
2024-07-30 18:04:04 +0200

    [feat]: loaded color rows

--- applications/frontend/ui/views/Game/Game.tsx
+++ applications/frontend/ui/views/Game/Game.tsx
@@ -1,36 +1,51 @@
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useShallow } from 'zustand/react/shallow'
 import { HeaderGame } from '@frontend/ui/components/HeaderGame/HeaderGame'
 import { Bonus } from '@frontend/ui/components/Bonus/Bonus'
-import { GameChip } from '@frontend/ui/components/GameChip/GameChip'
 import { useStore } from '@frontend/adapter/store/useStore'
 import { ColorMixDebug } from '@frontend/ui/components/ColorMixDebug/ColorMixDebug'
+import { Colors } from '@gameContext/shared/infrastructure/store/store'
+import { ColorsRow } from '@frontend/ui/components/ColorRow/ColorRow'
 import './Game.scss'
 
 const GameView = () => {
+  const [colors, setColors] = useState<Colors>()
   const navigate = useNavigate()
-  const { player, mixColor } = useStore(
-    useShallow(({ player, mixColor }) => ({
+  const { player, generateColors } = useStore(
+    useShallow(({ player, generateColors }) => ({
       player,
-      mixColor,
+      generateColors,
     })),
   )
+
+  useEffect(() => {
+    const loadColors = async () => {
+      const colors = await generateColors()
+      setColors(colors)
+    }
+
+    loadColors()
+  }, [])
+
+  const resultColors = colors?.items.map((color) => color.resultColor)
+  const subtractedColors = colors?.items.map((color) => color.subtractedColor)
 
   return (
     <article className="game view">
       <HeaderGame
-        level={player.level}
+        level={player.levelIndex + 1}
         lives={player.lives}
         score={player.score}
         onBack={() => navigate('/')}
       />
 
+      {resultColors && <ColorsRow colors={resultColors} />}
       <div className="game__divider"></div>
+      {subtractedColors && <ColorsRow colors={subtractedColors} />}
 
       <section className="game__footer">
-        <div className="game__chip">
-          <GameChip />
-        </div>
+        <div className="game__chip"></div>
 
         {player.bonus > 0 && (
           <div className="game__bonus">
@@ -38,7 +53,7 @@ const GameView = () => {
           </div>
         )}
 
-        <ColorMixDebug mixColor={mixColor} />
+        {colors && <ColorMixDebug colors={colors} />}
       </section>
     </article>
   )
