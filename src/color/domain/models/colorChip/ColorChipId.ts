625e7af358cedc616220ddcf007aa35004c2baef
ZenekeZene <hectorvillarm@gmail.com>
2024-08-01 00:50:16 +0200

    [refactor]: converted ColorChip entity to vo

--- src/color/domain/models/colorChip/ColorChipId.ts
+++
@@ -1,8 +0,0 @@
-import { Uuid } from '@gameContext/shared/domain/utils/Uuid'
-
-export class ColorChipId extends Uuid {
-  constructor(id?: string) {
-    const uuid = id || Uuid.random().valueOf()
-    super(uuid)
-  }
-}
