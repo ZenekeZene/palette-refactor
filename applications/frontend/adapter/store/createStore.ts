6b66b7fb6563d05107a23a51e512f57a7c8ba3bc
ZenekeZene <hectorvillarm@gmail.com>
2024-08-08 17:58:56 +0200

    [refactor]: splitted substore stypes

--- applications/frontend/adapter/store/createStore.ts
+++ applications/frontend/adapter/store/createStore.ts
@@ -3,9 +3,9 @@ import type { Store } from './types/store'
 import { createColorStore } from './slices/colorStore/colorStore'
 import { createQuoteStore } from './slices/quoteStore/quoteStore'
 import { createPlayerStore } from './slices/playerStore/playerStore'
-import type { PlayerStoreProps } from './slices/playerStore/playerStore.d'
+import type { PlayerStoreState } from './slices/playerStore/playerStore.d'
 
-export const createStore = (initialProps: PlayerStoreProps) => {
+export const createStore = (initialProps: PlayerStoreState) => {
   const createPlayerStoreWithInitialProps = createPlayerStore(initialProps)
   return create<Store>()((...args) => ({
     ...createQuoteStore(...args),
