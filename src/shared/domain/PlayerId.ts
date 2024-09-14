9d90c67e1f0772c7fb6d7a68d5cf190309e23250
ZenekeZene <hectorvillarm@gmail.com>
2024-09-14 11:38:32 +0200

    [refactor]: moved PlayerId to shared

---
+++ src/shared/domain/PlayerId.ts
@@ -0,0 +1,8 @@
+import { Uuid } from '@gameContext/shared/domain/utils/Uuid'
+
+export class PlayerId extends Uuid {
+  constructor(id?: string) {
+    const uuid = id || Uuid.random().valueOf()
+    super(uuid)
+  }
+}
