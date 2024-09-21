9ee6d0ce7e2c6b0b9edb151c2d5536a95f2f8867
ZenekeZene <hectorvillarm@gmail.com>
2024-09-21 10:59:32 +0200

    [feat]: added findByPlayerIdAndLevelId method in color repo

--- src/color/domain/repositories/ColorRepository.ts
+++ src/color/domain/repositories/ColorRepository.ts
@@ -1,11 +1,17 @@
+import { PlayerId } from '@gameContext/shared/domain/PlayerId'
 import { ColorGroupCollection } from '../ColorGroupCollection'
 import { ColorGroupCollectionId } from '../ColorGroupCollectionId'
 import { ColorGroupId } from '../models/colorGroup/ColorGroupId'
+import { LevelId } from '@gameContext/shared/domain/LevelId'
 
 export interface ColorRepository {
   save(colorGroupCollection: ColorGroupCollection): void
   findById(id: ColorGroupCollectionId): ColorGroupCollection | undefined
   findByColorGroupId(
     colorGroupId: ColorGroupId,
+  ): ColorGroupCollection | undefined
+  findByPlayerIdAndLevelId(
+    playerId: PlayerId,
+    levelId: LevelId,
   ): ColorGroupCollection | undefined
 }
--- src/color/infrastructure/ColorInMemoryRepository.ts
+++ src/color/infrastructure/ColorInMemoryRepository.ts
@@ -3,6 +3,8 @@ import type { ColorGroupCollection } from '../domain/ColorGroupCollection'
 import type { ColorGroupCollectionId } from '../domain/ColorGroupCollectionId'
 import type { ColorRepository } from '../domain/repositories/ColorRepository'
 import { ColorGroupId } from '../domain/models/colorGroup/ColorGroupId'
+import { LevelId } from '@gameContext/shared/domain/LevelId'
+import { PlayerId } from '@gameContext/shared/domain/PlayerId'
 
 @injectable()
 export class ColorInMemoryRepository implements ColorRepository {
@@ -10,6 +12,15 @@ export class ColorInMemoryRepository implements ColorRepository {
 
   private transformToArray(): [string, ColorGroupCollection][] {
     return Array.from(this.colorGroupCollections)
+  }
+
+  private find(
+    callback: (colorGroupCollection: ColorGroupCollection) => void,
+  ): ColorGroupCollection | undefined {
+    const entries = this.transformToArray()
+    return entries.findLast(([, colorGroupCollection]) =>
+      callback(colorGroupCollection),
+    )?.[1]
   }
 
   save(colorGroupCollection: ColorGroupCollection): void {
@@ -26,12 +37,21 @@ export class ColorInMemoryRepository implements ColorRepository {
   findByColorGroupId(
     colorGroupId: ColorGroupId,
   ): ColorGroupCollection | undefined {
-    const entries = this.transformToArray()
-    const colorGroupCollection = entries.find(([, colorGroupCollection]) =>
+    return this.find((colorGroupCollection) =>
       colorGroupCollection.items.some((colorGroup) =>
         colorGroup.id.equals(colorGroupId),
       ),
-    )?.[1]
-    return colorGroupCollection
+    )
+  }
+
+  findByPlayerIdAndLevelId(
+    playerId: PlayerId,
+    levelId: LevelId,
+  ): ColorGroupCollection | undefined {
+    return this.find(
+      (colorGroupCollection) =>
+        colorGroupCollection.playerId.equals(playerId) &&
+        colorGroupCollection.levelId.equals(levelId),
+    )
   }
 }
