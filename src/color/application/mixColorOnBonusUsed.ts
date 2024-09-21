3ef587e076636d6bfed9b88dffcdf37a4d409df2
ZenekeZene <hectorvillarm@gmail.com>
2024-09-21 11:06:01 +0200

    [feat]: added mix color on bonus used

---
+++ src/color/application/mixColorOnBonusUsed.ts
@@ -0,0 +1,40 @@
+import { inject, injectable } from 'tsyringe'
+import { Types } from '@frontend/infrastructure/dependency-injection/identifiers'
+import type { EventBus } from '@gameContext/shared/domain/utils/EventBus'
+import { BonusUsedEvent } from '@gameContext/player/domain/events/BonusUsedEvent'
+import { DomainEvent } from '@gameContext/shared/domain/utils/DomainEvent'
+import { DomainEventSubscriber } from '@gameContext/shared/domain/utils/DomainEventSubscriber'
+import { Class } from '@gameContext/shared/types/Class'
+import { AggregateRoot } from '@gameContext/shared/domain/utils/AggregateRoot'
+import { ColorGroupCollectionNotFound } from '../domain/exceptions/ColorGroupCollectionNotFound'
+import type { ColorRepository } from '../domain/repositories/ColorRepository'
+
+@injectable()
+export class MixColorOnBonusUsed
+  implements DomainEventSubscriber<BonusUsedEvent>
+{
+  constructor(
+    @inject(Types.ColorRepository) private colorRepository: ColorRepository,
+    @inject(Types.EventBus) private eventBus: EventBus,
+  ) {}
+
+  subscribedTo(): Class<DomainEvent<AggregateRoot>>[] {
+    return [BonusUsedEvent]
+  }
+
+  async on(domainEvent: BonusUsedEvent): Promise<void> {
+    const { aggregate } = domainEvent
+    // TODO: domain event should have primitive values
+    const { id: playerId, levelId } = aggregate
+    const colorGroupCollection = this.colorRepository.findByPlayerIdAndLevelId(
+      playerId,
+      levelId,
+    )
+    if (!colorGroupCollection) {
+      throw ColorGroupCollectionNotFound.ofPlayerIdAndLevelId(playerId, levelId)
+    }
+    colorGroupCollection.mixColorGroupPending()
+    this.colorRepository.save(colorGroupCollection)
+    this.eventBus.publish(colorGroupCollection.pullDomainEvents())
+  }
+}
