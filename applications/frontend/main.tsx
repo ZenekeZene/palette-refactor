0de30b25d7548a02ac53553df968ef654f907da6
ZenekeZene <hectorvillarm@gmail.com>
2024-09-21 10:22:18 +0200

    [tools]: removed React.StrictMode. setUseWhatChange in dev mode

--- applications/frontend/main.tsx
+++ applications/frontend/main.tsx
@@ -1,11 +1,13 @@
 import 'reflect-metadata'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { configureDependencies } from '@frontend/infrastructure/dependency-injection/container'
 import { RouterProvider } from '@frontend/adapter/router/RouterProvider'
 import { configureEventBus } from '@frontend/infrastructure/eventBus'
 import { configureStore } from '@frontend/adapter/store/useStore'
+import { setUseWhatChange } from '@simbathesailor/use-what-changed'
 import '@frontend/ui/styles/index.css'
+
+setUseWhatChange(process.env.NODE_ENV === 'development')
 
 async function init() {
   configureDependencies()
@@ -13,10 +15,13 @@ async function init() {
   await configureStore()
 
   ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <RouterProvider />
-    </React.StrictMode>,
+    // (1)
+    // <React.StrictMode>
+    <RouterProvider />,
+    // </React.StrictMode>,
   )
 }
 
 init()
+
+// (1): Launches twice the save in the color repository.
